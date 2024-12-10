fb7f104b83453b6544ec717adf041fcbb03faa47
Alejandro Quiles García <alexqg1208@gmail.com>
2024-12-10 13:01:20 +0000

    First Commit

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Counter = ({ units, tens, hundreds, thousands, tenThousands, hundredThousands }) => {
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="d-flex">
+
+                <div className="digit box">{hundredThousands}</div>
+                <div className="digit box">{tenThousands}</div>
+                <div className="digit box">{thousands}</div>
+                <div className="digit box">{hundreds}</div>
+                <div className="digit box">{tens}</div>
+                <div className="digit box">{units}</div>
+            </div>
+        </div>
+    );
+};
